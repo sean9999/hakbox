6c7aa472cae7471ef8980a7dbfee1a0f0dd730b3
Sean M <Sean.macdonald@hardrockdigital.com>
2024-05-11 23:53:43 -0400

    remove expensive rust

--- peter.sh
+++ peter.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-STARTCOLOR='\[\033[0;32m\]'   
+STARTCOLOR='\[\033[01;31m\]'   
 ENDCOLOR="\e[0m"
 export PS1="\n\[$STARTCOLOR\]peter \w\n\$ \[$ENDCOLOR\]"
 source /etc/environment
-#source /root/.cargo/env
-#alias cat='bat -pp'
