9ecf65713ea14b47530a7bf735e8d25e9be077e0
Sean Macdonald <sean.r.macdonald@gmail.com>
2024-05-12 21:38:28 -0400

    add arch

--- build.sh
+++ build.sh
@@ -3,7 +3,9 @@
 source vars.sh
 
 docker buildx build \
-    --platform="linux/amd64,linux/arm64,linux/arm/v7" \
+    --platform="linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v8" \
     -t hakbox \
     -t $IMAGE:latest \
-    -t $IMAGE:$TAG .
+    -t $IMAGE:$TAG . \
+    --output=type=registry,push=true
+
